a494dd04efee3c865d9ad6131538fd938138e742
Ulysses Atkeson <uatkeson19@kentdenver.org>
2018-11-07 15:08:03 -0700

    add task 4

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Ulysses Atkeson, 11/7/18, Tree Quiz Hashtable
-
+import unittest
 class HashTable:
     def __init__(self, capacity):
         self.cap = capacity
@@ -24,16 +24,51 @@ class HashTable:
         self.arr = tempArr
         self.cap = newSize
 
+class TestMethods(unittest.TestCase):
+    def test_init(self):#
+        print("Running init test")
+        h = HashTable(1000)
+        self.assertEqual(h.get("0"), None)
+        print("init test complete\n")
+
+    def test_put_and_get(self):
+        print("Running put and get test")
+        h = HashTable(1000)
+        self.assertEqual(h.get("test"), None)
+        h.put("test", "ing")
+        h.put("do", "thing")
+        self.assertEqual(h.get("test"), [["test","ing"]])
+        self.assertEqual(h.get("idk"), None)
+        print("put and get test complete\n")
+
+    def test_task1(self):
+        print("Running task 1 test")
+        h = HashTable(10)
+        h.put("test", 1)
+        h.resize(20)
+        self.assertEqual(h.get("test"), [["test", 1]])
+        h.resize(1)
+        self.assertEqual(h.get("test"), [["test", 1]])
+        print("task 1 test complete\n")
+
+    def test_task2(self):
+        print("Running task 2 test")
+        h = HashTable(10)
+        h.put("test", 1)
+        h.put("test", 2)
+        self.assertEqual(h.get("test"), [["test", 1], ["test", 2]])
+        print("task 2 test complete\n")
+    
+    def test_task3(self):
+        print("Running task 3 test")
+        h = HashTable(5)
+        h.put("test", 1)
+        h.put("ing", 2)
+        h.put("hey", 3)
+        self.assertEqual(h.cap, 5)
+        h.put("troll", 4)
+        self.assertEqual(h.cap, 10)
+        print("task 3 test complete\n")
 
 if __name__ == '__main__':
-  h = HashTable(10)
-  h.put("hi", 20)
-  h.put("hi", 30)
-  h.put("hello", 30)
-  h.put("test", 30)
-  h.put("ing", 30)
-  h.put("yo", 30)
-  h.put("lo", 30)
-  print(h.arr)
-  h.put("hey", 30)
-  print(h.arr)
+  unittest.main()#task 4
